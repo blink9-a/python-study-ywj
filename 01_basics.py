dd6b058a174e16f563d81fce608b6265d3b7d272
blink9-a <325894332+blink9-a@users.noreply.github.com>
2026-09-08 23:48:31 +0900

    파이썬 기초 실습 코드 추가 (출력, 변수, 주석)

---
+++ 01_basics.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+print(3 + 3)
+print(2 * 5)
+
+#print("Hello, World!")
+
+x = 5
+y = "John"
+print(x)
+print(y)
