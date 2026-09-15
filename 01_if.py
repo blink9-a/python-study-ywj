62161aeb25410fade0709f548b42bfd9079c1b99
blink9-a <325894332+blink9-a@users.noreply.github.com>
2026-09-16 01:47:49 +0900

    입출력 및 형변환, 조건문 기초 실습 코드 추가

---
+++ 01_if.py
@@ -0,0 +1,23 @@
+name = input("이름을 입력하세요: ")
+print(name)
+
+age = input("나이를 입력하세요: ")
+age2 = int(age)
+print(age2)
+
+score = 90
+if score >= 80:
+    print("합격")
+
+
+
+
+
+a = "전사"
+print("게임 직업을 선택하세요 " + a)
+b = input("게임 직업을 선택하세요 ")
+
+print("검을 받았습니다." + a)
+
+
+
